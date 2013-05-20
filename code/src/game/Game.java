e4851fc4f36b40b401456385eaf1ffd512644804
Corey Frenette <corey@narwhunderful.com>
2013-05-20 09:00:06 -0300

    Game: singleton, sprite loading

--- code/src/game/Game.java
+++ code/src/game/Game.java
@@ -1,10 +1,35 @@
 package game;
 
+import java.util.HashMap;
+
+import game.data.Sprite;
 import game.network.Server;
 import game.settings.Settings;
 
 public class Game {
+  private static Game _instance = new Game();
+  
+  public static Game getInstance() { return _instance; }
+  
   private Server _net;
+  
+  private HashMap<String, Sprite> _sprite = new HashMap<String, Sprite>();
+  
+  public Sprite getSprite(String file) {
+    Sprite s = _sprite.get(file);
+    
+    if(s == null) {
+      if((s = new Sprite(file)).load()) {
+        System.out.println("Sprite " + file + " loaded.");
+      } else {
+        System.err.println("Couldn't load sprite " + file);
+      }
+      
+      _sprite.put(file, s);
+    }
+    
+    return s;
+  }
   
   public void start() {
     Settings.init();
