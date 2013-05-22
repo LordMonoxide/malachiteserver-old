0dc78abb37b09b6c84aa56d6a7e2fe26cf30d711
Corey Frenette <corey@narwhunderful.com>
2013-05-22 13:33:05 -0300

    Character: add sprite field

--- code/src/game/data/account/Character.java
+++ code/src/game/data/account/Character.java
@@ -5,6 +5,7 @@ public class Character {
   private Account _account;
   private String _name;
   private String _world;
+  private String _sprite;
   private float _x, _y;
   private int _z;
   
@@ -17,6 +18,7 @@ public class Character {
   public Account getAccount() { return _account; }
   public String  getName()    { return _name; }
   public String  getWorld()   { return _world; }
+  public String  getSprite()  { return _sprite; }
   public float   getX()       { return _x; }
   public float   getY()       { return _y; }
   public int     getZ()       { return _z; }
@@ -25,6 +27,7 @@ public class Character {
   public void setAccount(Account account) { _account = account; }
   public void setName   (String name)     { _name = name; }
   public void setWorld  (String world)    { _world = world; }
+  public void setSprite (String sprite)   { _sprite = sprite; }
   public void setX      (float x)         { _x = x; }
   public void setY      (float y)         { _y = y; }
   public void setZ      (int z)           { _z = z; }
