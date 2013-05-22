be6639a0754e56ce358bd3189849ad785156f684
Corey Frenette <corey@narwhunderful.com>
2013-05-22 13:35:22 -0300

    World: add bulk Entity add/remove functions, maintain list of Connections, add send(Packet) function

--- code/src/game/world/World.java
+++ code/src/game/world/World.java
@@ -1,10 +1,14 @@
 package game.world;
 
 import game.data.Map;
+import game.network.Connection;
+import game.network.packet.EntityCreate;
 
 import java.io.File;
 import java.util.HashMap;
 import java.util.LinkedList;
+
+import network.packet.Packet;
 
 import physics.Sandbox;
 
@@ -13,6 +17,7 @@ public class World extends Sandbox {
   private HashMap<String, Map> _map = new HashMap<String, Map>();
   
   private LinkedList<Entity> _entity = new LinkedList<Entity>();
+  private LinkedList<Connection> _connection = new LinkedList<Connection>();
   
   private String _name;
   
@@ -56,16 +61,44 @@ public class World extends Sandbox {
     return r;
   }
   
+  public void addEntity(Entity[] e) {
+    for(Entity entity : e) {
+      addEntity(entity);
+    }
+  }
+  
   public void addEntity(Entity e) {
     e.setWorld(this);
     e.setRegion(getRegion(e.getMX(), e.getMY()));
     _entity.add(e);
     addToSandbox(e);
+    
+    if(e.getConnection() != null) {
+      _connection.add(e.getConnection());
+    }
+    
+    send(new EntityCreate(e));
+  }
+  
+  public void removeEntity(Entity[] e) {
+    for(Entity entity : e) {
+      removeEntity(entity);
+    }
   }
   
   public void removeEntity(Entity e) {
+    if(e.getConnection() != null) {
+      _connection.remove(e.getConnection());
+    }
+    
     removeFromSandbox(e);
     _entity.remove(e);
     e.setWorld(null);
+  }
+  
+  public void send(Packet packet) {
+    for(Connection c : _connection) {
+      c.send(packet);
+    }
   }
 }
