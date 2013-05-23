c2a9b753385911ddb3575cc3df89b8ee9c13e6d2
Corey Frenette <corey@narwhunderful.com>
2013-05-23 17:01:48 -0300

    EntityMoveStop packet

---
+++ code/src/game/network/packet/EntityMoveStop.java
@@ -0,0 +1,45 @@
+package game.network.packet;
+
+import game.network.Connection;
+import game.world.Entity;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import network.packet.Packet;
+
+public class EntityMoveStop extends Packet {
+  private int _id;
+  private float _x, _y;
+  
+  public EntityMoveStop() { }
+  public EntityMoveStop(Entity e) {
+    _id = e.getID();
+    _x  = e.getX();
+    _y  = e.getY();
+  }
+  
+  public int getIndex() {
+    return 13;
+  }
+  
+  public ByteBuf serialize() {
+    ByteBuf b = Unpooled.buffer();
+    b.writeInt(_id);
+    b.writeFloat(_x);
+    b.writeFloat(_y);
+    return b;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    _x    = data.readFloat();
+    _y    = data.readFloat();
+  }
+  
+  public void process() {
+    Connection c = (Connection)_connection;
+    Entity e = c.getEntity();
+    e.setX(_x);
+    e.setY(_y);
+    e.stopMoving();
+    e.getWorld().send(new EntityMoveStop(e));
+  }
+}
