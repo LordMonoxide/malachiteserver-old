fc81671c94060a8d8d5f54f26c1edff1d66f351b
Corey Frenette <corey@narwhunderful.com>
2013-06-06 20:01:32 -0300

    New data packet for syncing any data

--- code/src/game/network/packet/Data.java
+++ code/src/game/network/packet/Data.java
@@ -10,77 +10,75 @@ import io.netty.buffer.Unpooled;
 import network.packet.Packet;
 
 public class Data {
-  public static final byte DATA_TYPE_MAP    = 1;
+  public static final byte DATA_TYPE_MAP = 1;
   public static final byte DATA_TYPE_SPRITE = 2;
+  
+  public static class Info extends Packet {
+    private Serializable[] _data;
+    
+    public Info(Serializable[] data) {
+      setData(data);
+    }
+    
+    public int getIndex() {
+      return 19;
+    }
+    
+    public void setData(Serializable[] data) {
+      _data = data;
+    }
+    
+    public ByteBuf serialize() {
+      ByteBuf b = Unpooled.buffer();
+      
+      if(_data[0] instanceof Sprite) {
+        b.writeByte(DATA_TYPE_SPRITE);
+      }
+      
+      b.writeInt(_data.length);
+      
+      for(Serializable data : _data) {
+        b.writeShort(data.getFile().length());
+        b.writeBytes(data.getFile().getBytes());
+        b.writeInt(data.getCRC());
+      }
+      
+      return b;
+    }
+    
+    public void deserialize(ByteBuf data) throws NotEnoughDataException {
+      
+    }
+    
+    public void process() {
+      
+    }
+  }
   
   public static class Request extends Packet {
     private byte _type;
-    private int _x, _y;
     private String _file;
-    private int _crc;
-    
-    public Request() { }
-    public Request(Serializable data) {
-      if(data instanceof Map) {
-        Map m = (Map)data;
-        _type = DATA_TYPE_MAP;
-        _x = m.getX();
-        _y = m.getY();
-      }
-      
-      if(data instanceof Sprite) {
-        _type = DATA_TYPE_SPRITE;
-        _file = data.getFile();
-      }
-      
-      _crc = data.getCRC();
-    }
     
     public int getIndex() {
       return 14;
     }
     
     public ByteBuf serialize() {
-      ByteBuf b = Unpooled.buffer();
-      b.writeByte(_type);
-      
-      if(_type == DATA_TYPE_MAP) {
-        b.writeInt(_x);
-        b.writeInt(_y);
-      } else {
-        b.writeShort(_file.length());
-        b.writeBytes(_file.getBytes());
-      }
-      
-      b.writeInt(_crc);
-      return b;
+      return null;
     }
     
     public void deserialize(ByteBuf data) throws NotEnoughDataException {
       _type = data.readByte();
-      
-      if(_type == DATA_TYPE_MAP) {
-        _x = data.readInt();
-        _y = data.readInt();
-      } else {
-        byte[] arr = new byte[data.readShort()];
-        data.readBytes(arr);
-        _file = new String(arr);
-      }
-      
-      _crc = data.readInt();
+      byte[] arr = new byte[data.readShort()];
+      data.readBytes(arr);
+      _file = new String(arr);
     }
     
     public void process() {
       switch(_type) {
-        case DATA_TYPE_MAP:
-          Map m = ((Connection)_connection).getEntity().getWorld().getRegion(_x, _y).getMap();
-          _connection.send(new Response(m, m.getCRC() != _crc));
-          break;
-          
         case DATA_TYPE_SPRITE:
           Sprite data = Game.getInstance().getSprite(_file);
-          _connection.send(new Response(data, data.getCRC() != _crc));
+          _connection.send(new Response(data));
           break;
       }
     }
@@ -88,25 +86,15 @@ public class Data {
   
   public static class Response extends Packet {
     private byte _type;
-    private int _x, _y;
     private String _file;
     private byte[] _data;
     
     public Response() { }
-    public Response(Serializable data, boolean needed) {
-      if(data instanceof Map) {
-        Map m = (Map)data;
-        _type = DATA_TYPE_MAP;
-        _x = m.getX();
-        _y = m.getY();
-      }
+    public Response(Serializable data) {
+      if(data instanceof Sprite) _type = DATA_TYPE_SPRITE;
       
-      if(data instanceof Sprite) {
-        _type = DATA_TYPE_SPRITE;
-        _file = data.getFile();
-      }
-      
-      if(needed) _data = data.serialize().serialize();
+      _file = data.getFile();
+      _data = data.serialize().serialize();
     }
     
     public int getIndex() {
@@ -116,14 +104,65 @@ public class Data {
     public ByteBuf serialize() {
       ByteBuf b = Unpooled.buffer();
       b.writeByte(_type);
+      b.writeShort(_file.length());
+      b.writeBytes(_file.getBytes());
+      b.writeInt(_data.length);
+      b.writeBytes(_data);
+      return b;
+    }
+    
+    public void deserialize(ByteBuf data) throws NotEnoughDataException {
       
-      if(_type == DATA_TYPE_MAP) {
-        b.writeInt(_x);
-        b.writeInt(_y);
-      } else {
-        b.writeShort(_file.length());
-        b.writeBytes(_file.getBytes());
-      }
+    }
+    
+    public void process() {
+      
+    }
+  }
+  
+  public static class MapRequest extends Packet {
+    private int _x, _y;
+    private int _crc;
+    
+    public int getIndex() {
+      return 18;
+    }
+    
+    public ByteBuf serialize() {
+      return null;
+    }
+    
+    public void deserialize(ByteBuf data) throws NotEnoughDataException {
+      _x = data.readInt();
+      _y = data.readInt();
+      _crc = data.readInt();
+    }
+    
+    public void process() {
+      Map m = ((Connection)_connection).getEntity().getWorld().getRegion(_x, _y).getMap();
+      _connection.send(new MapResponse(m, m.getCRC() != _crc));
+    }
+  }
+  
+  public static class MapResponse extends Packet {
+    private int _x, _y;
+    private byte[] _data;
+    
+    public MapResponse() { }
+    public MapResponse(Map data, boolean needed) {
+      _x = data.getX();
+      _y = data.getY();
+      if(needed) _data = data.serialize().serialize();
+    }
+    
+    public int getIndex() {
+      return 20;
+    }
+    
+    public ByteBuf serialize() {
+      ByteBuf b = Unpooled.buffer();
+      b.writeInt(_x);
+      b.writeInt(_y);
       
       if(_data != null) {
         b.writeInt(_data.length);
