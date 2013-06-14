f1cec227341a59e5140ed8ec788881219500f7d8
Corey Frenette <corey@narwhunderful.com>
2013-06-14 18:06:50 -0300

    Add Type and Damage to Item and bump VERSION to 2

--- code/src/game/data/Item.java
+++ code/src/game/data/Item.java
@@ -7,10 +7,12 @@ import game.data.util.Data;
 import game.data.util.Serializable;
 
 public class Item extends Serializable implements Data {
-  private static final int VERSION = 1;
+  private static final int VERSION = 2;
   
   private String _name, _note;
   private String _sprite;
+  private Type   _type = Type.None;
+  private int    _damage;
   
   public Item(File file) {
     super(file);
@@ -19,6 +21,8 @@ public class Item extends Serializable implements Data {
   public String getName()   { return _name; }
   public String getNote()   { return _note; }
   public String getSprite() { return _sprite; }
+  public Type   getType()   { return _type; }
+  public int    getDamage() { return _damage; }
   
   public Buffer serialize() {
     Buffer b = new Buffer();
@@ -26,18 +30,53 @@ public class Item extends Serializable implements Data {
     b.put(_name);
     b.put(_note);
     b.put(_sprite);
+    b.put(_type.ordinal());
+    b.put(_damage);
     return b;
   }
   
   public void deserialize(Buffer b) {
     switch(b.getInt()) {
-      case 1: deserialize01(b);
+      case 1: deserialize01(b); break;
+      case 2: deserialize02(b); break;
     }
   }
   
   private void deserialize01(Buffer b) {
-    _name = b.getString();
-    _note = b.getString();
+    _name   = b.getString();
+    _note   = b.getString();
     _sprite = b.getString();
+  }
+  
+  private void deserialize02(Buffer b) {
+    _name   = b.getString();
+    _note   = b.getString();
+    _sprite = b.getString();
+    _type   = Type.valueOf(b.getInt());
+    _damage = b.getInt();
+  }
+  
+  public enum Type {
+    None, Potion, Melee, Bow, Shield, Spell, Body, Head, Hand, Legs, Feet, Ring, Amulet;
+    
+    public static Type valueOf(int index) {
+      switch(index) {
+        case  0: return None;
+        case  1: return Potion;
+        case  2: return Melee;
+        case  3: return Bow;
+        case  4: return Shield;
+        case  5: return Spell;
+        case  6: return Body;
+        case  7: return Head;
+        case  8: return Hand;
+        case  9: return Legs;
+        case 10: return Feet;
+        case 11: return Ring;
+        case 12: return Amulet;
+      }
+      
+      return null;
+    }
   }
 }
