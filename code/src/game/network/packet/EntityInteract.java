69809823743dc5f4a928f46a31159e624281e107
Corey Frenette <corey@narwhunderful.com>
2013-06-13 21:49:20 -0300

    EntityInteract Packet

---
+++ code/src/game/network/packet/EntityInteract.java
@@ -0,0 +1,55 @@
+package game.network.packet;
+
+import game.data.Item;
+import game.network.Connection;
+import game.world.Entity;
+import io.netty.buffer.ByteBuf;
+import network.packet.Packet;
+
+public class EntityInteract extends Packet {
+  private int _id;
+  
+  public int getIndex() {
+    return 27;
+  }
+  
+  public ByteBuf serialize() {
+    return null;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    _id = data.readInt();
+  }
+  
+  public void process() {
+    Connection c = (Connection)_connection;
+    Entity e = c.getEntity().getWorld().getEntity(_id);
+    
+    if(e == null) {
+      c.kick("Invalid entity id");
+      return;
+    }
+    
+    if(e.getZ() != c.getEntity().getZ()) {
+      c.kick("Wrong layer");
+      return;
+    }
+    
+    switch(e.getType()) {
+      case Item:
+        if(e.isCloseTo(c.getEntity())) {
+          //TODO: VALUE
+          Entity.Inv inv = c.getEntity().giveItem((Item)e.getData(), 1);
+          
+          if(inv != null) {
+            e.remove();
+            c.send(new EntityInvUpdate(c.getEntity(), inv));
+          }
+        }
+        
+        break;
+        
+      default:
+    }
+  }
+}
