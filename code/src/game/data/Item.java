09b18ae493f063f97626aa9f0cfacf0bb47e9bd3
Corey Frenette <corey@narwhunderful.com>
2013-06-15 13:39:13 -0300

    Compress bit pattern

--- code/src/game/data/Item.java
+++ code/src/game/data/Item.java
@@ -52,35 +52,38 @@ public class Item extends Serializable implements Data {
   /*  0000 0000 0000 0000 0000 0000 0000 0000
    *  ^     ATTRIBS     ^ ^  SUB  ^ ^ TYPES ^
    */
-  public static final int ITEM_TYPE_BITMASK             = 0x000000FF;
+  public static final int ITEM_TYPE_BITMASK             = 0x0000000F;
   public static final int ITEM_TYPE_BITSHIFT            = 0;
   
-  public static final int ITEM_SUBTYPE_BITMASK          = 0x0000FF00;
+  public static final int ITEM_SUBTYPE_BITMASK          = 0x000000F0;
   public static final int ITEM_SUBTYPE_BITSHIFT         = 4;
   
+  public static final int ITEM_ATTRIBS_BITMASK          = 0xFFFFFF00;
+  public static final int ITEM_ATTRIBS_BITSHIFT         = 8;
+  
   public static final int ITEM_TYPE_WEAPON              = 0x01;
-  public static final int ITEM_TYPE_WEAPON_MELEE        = 0x00 * 0x100;
-  public static final int ITEM_TYPE_WEAPON_BOW          = 0x01 * 0x100;
-  public static final int ITEM_TYPE_WEAPON_SHIELD       = 0x02 * 0x100;
+  public static final int ITEM_TYPE_WEAPON_MELEE        = 0x00 * 0x10;
+  public static final int ITEM_TYPE_WEAPON_BOW          = 0x01 * 0x10;
+  public static final int ITEM_TYPE_WEAPON_SHIELD       = 0x02 * 0x10;
   
   public static final int ITEM_TYPE_SHIELD              = 0x02;
   
   public static final int ITEM_TYPE_ARMOUR              = 0x03;
-  public static final int ITEM_TYPE_ARMOUR_BODY         = 0x00 * 0x100;
-  public static final int ITEM_TYPE_ARMOUR_HEAD         = 0x01 * 0x100;
-  public static final int ITEM_TYPE_ARMOUR_HAND         = 0x02 * 0x100;
-  public static final int ITEM_TYPE_ARMOUR_LEGS         = 0x03 * 0x100;
-  public static final int ITEM_TYPE_ARMOUR_FEET         = 0x04 * 0x100;
+  public static final int ITEM_TYPE_ARMOUR_BODY         = 0x00 * 0x10;
+  public static final int ITEM_TYPE_ARMOUR_HEAD         = 0x01 * 0x10;
+  public static final int ITEM_TYPE_ARMOUR_HAND         = 0x02 * 0x10;
+  public static final int ITEM_TYPE_ARMOUR_LEGS         = 0x03 * 0x10;
+  public static final int ITEM_TYPE_ARMOUR_FEET         = 0x04 * 0x10;
   
   public static final int ITEM_TYPE_POTION              = 0x04;
-  public static final int ITEM_TYPE_POTION_HEAL         = 0x00 * 0x100;
-  public static final int ITEM_TYPE_POTION_HEAL_FIXED   = 0x00 * 0x10000;
-  public static final int ITEM_TYPE_POTION_HEAL_PERCENT = 0x00 * 0x10000;
-  public static final int ITEM_TYPE_POTION_BUFF         = 0x01 * 0x100;
+  public static final int ITEM_TYPE_POTION_HEAL         = 0x00 * 0x10;
+  public static final int ITEM_TYPE_POTION_HEAL_FIXED   = 0x00 * 0x100;
+  public static final int ITEM_TYPE_POTION_HEAL_PERCENT = 0x00 * 0x100;
+  public static final int ITEM_TYPE_POTION_BUFF         = 0x01 * 0x10;
   
   public static final int ITEM_TYPE_SPELL               = 0x05;
   
   public static final int ITEM_TYPE_BLING               = 0x06;
-  public static final int ITEM_TYPE_BLING_RING          = 0x00 * 0x100;
-  public static final int ITEM_TYPE_BLING_AMULET        = 0x01 * 0x100;
+  public static final int ITEM_TYPE_BLING_RING          = 0x00 * 0x10;
+  public static final int ITEM_TYPE_BLING_AMULET        = 0x01 * 0x10;
 }
