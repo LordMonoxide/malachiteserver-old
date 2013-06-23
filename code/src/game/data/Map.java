9e281a32a19016bf95e27ed51ddb046f0a0c857a
Corey Frenette <corey@narwhunderful.com>
2013-06-23 10:48:39 -0300

    Add NPCs to Maps

--- code/src/game/data/Map.java
+++ code/src/game/data/Map.java
@@ -10,12 +10,13 @@ import game.settings.Settings;
 import game.world.Entity;
 
 public class Map extends Serializable {
-  private static final int VERSION = 4;
+  private static final int VERSION = 5;
   
   protected int _x, _y;
   protected Layer[]            _layer  = new Layer[Settings.Map.Depth()];
   protected LinkedList<Sprite> _sprite = new LinkedList<Sprite>();
   protected LinkedList<Item>   _item   = new LinkedList<Item>();
+  protected LinkedList<NPC>    _npc    = new LinkedList<NPC>();
   
   public Map(String world, int x, int y) {
     super(new File("../data/worlds/" + world + "/" + x + "x" + y));
@@ -80,6 +81,7 @@ public class Map extends Serializable {
     
     b.put(_sprite.size());
     b.put(_item.size());
+    b.put(_npc.size());
     
     for(int z = 0; z < _layer.length; z++) {
       for(int x = 0; x < _layer[z]._tile.length; x++) {
@@ -113,6 +115,13 @@ public class Map extends Serializable {
       b.put(i._z);
     }
     
+    for(NPC n : _npc) {
+      b.put(n._file);
+      b.put(n._x);
+      b.put(n._y);
+      b.put(n._z);
+    }
+    
     return b;
   }
   
@@ -122,6 +131,7 @@ public class Map extends Serializable {
       case 2: deserialize02(b); break;
       case 3: deserialize03(b); break;
       case 4: deserialize04(b); break;
+      case 5: deserialize05(b); break;
     }
   }
   
@@ -343,6 +353,82 @@ public class Map extends Serializable {
     }
   }
   
+  private void deserialize05(Buffer b) {
+    _sprite.clear();
+    _item.clear();
+    
+    _x = b.getInt();
+    _y = b.getInt();
+    
+    int sizeZ = b.getInt();
+    int sizeX = b.getInt();
+    int sizeY = b.getInt();
+    int sizeXA = b.getInt();
+    int sizeYA = b.getInt();
+    
+    int spriteSize = b.getInt();
+    int itemSize = b.getInt();
+    int npcSize = b.getInt();
+    
+    int maxX = sizeX > Settings.Map.Tile.Count() ? Settings.Map.Tile.Count() : sizeX;
+    int maxY = sizeY > Settings.Map.Tile.Count() ? Settings.Map.Tile.Count() : sizeY;
+    int maxZ = sizeZ > Settings.Map.Depth() ? Settings.Map.Depth() : sizeZ;
+    int maxXA = sizeXA > (Settings.Map.Attrib.Count()) ? (Settings.Map.Attrib.Count()) : sizeXA;
+    int maxYA = sizeYA > (Settings.Map.Attrib.Count()) ? (Settings.Map.Attrib.Count()) : sizeYA;
+    
+    _layer = new Layer[Settings.Map.Depth()];
+    
+    for(int z = 0; z < maxZ; z++) {
+      _layer[z] = new Layer();
+      _layer[z]._tile = new Tile[Settings.Map.Tile.Count()][Settings.Map.Tile.Count()];
+      _layer[z]._attrib = new Attrib[Settings.Map.Attrib.Count()][Settings.Map.Attrib.Count()];
+      
+      for(int x = 0; x < maxX; x++) {
+        for(int y = 0; y < maxY; y++) {
+          _layer[z]._tile[x][y] = new Tile();
+          _layer[z]._tile[x][y]._x = b.getByte();
+          _layer[z]._tile[x][y]._y = b.getByte();
+          _layer[z]._tile[x][y]._tileset = b.getByte();
+          _layer[z]._tile[x][y]._a = b.getByte();
+        }
+      }
+      
+      for(int x = 0; x < maxXA; x++) {
+        for(int y = 0; y < maxYA; y++) {
+          _layer[z]._attrib[x][y] = new Attrib();
+          _layer[z]._attrib[x][y]._type = b.getByte();
+        }
+      }
+    }
+    
+    for(int i = 0; i < spriteSize; i++) {
+      Sprite s = new Sprite();
+      s._file = b.getString();
+      s._x = b.getInt();
+      s._y = b.getInt();
+      s._z = b.getByte();
+      _sprite.add(s);
+    }
+    
+    for(int i = 0; i < itemSize; i++) {
+      Item item = new Item();
+      item._file = b.getString();
+      item._val = b.getInt();
+      item._x = b.getInt();
+      item._y = b.getInt();
+      item._z = b.getByte();
+      _item.add(item);
+    }
+    
+    for(int i = 0; i < npcSize; i++) {
+      NPC n = new NPC();
+      n._file = b.getString();
+      n._x = b.getInt();
+      n._y = b.getInt();
+      n._z = b.getByte();
+    }
+  }
+  
   public class Layer {
     protected Tile[][] _tile = new Tile[Settings.Map.Tile.Count()][Settings.Map.Tile.Count()];
     protected Attrib[][] _attrib = new Attrib[Settings.Map.Attrib.Count()][Settings.Map.Attrib.Count()];
@@ -417,9 +503,14 @@ public class Map extends Serializable {
   }
   
   public static class Sprite extends Data {
+    
   }
   
   public static class Item extends Data {
     public int _val;
+  }
+  
+  public static class NPC extends Data {
+    
   }
 }
