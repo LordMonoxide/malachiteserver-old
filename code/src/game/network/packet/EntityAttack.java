862a0321b6a3bd850959e79a1b598d46fb22d155
Corey Frenette <corey@narwhunderful.com>
2013-08-08 17:13:41 -0300

    Add EntityAttack packet to be passed back to clients

--- code/src/game/network/packet/EntityAttack.java
+++ code/src/game/network/packet/EntityAttack.java
@@ -1,18 +1,38 @@
 package game.network.packet;
 
 import game.network.Connection;
+import game.world.Entity;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import network.packet.Packet;
 
 public class EntityAttack extends Packet {
   private double _angle;
   
+  private Entity _attacker, _defender;
+  private int _damage;
+  
+  public EntityAttack() { }
+  public EntityAttack(Entity attacker, Entity defender, int damage) {
+    _attacker = attacker;
+    _defender = defender;
+    _damage = damage;
+  }
+  
   public int getIndex() {
-    return 38;
+    return 39;
   }
   
   public ByteBuf serialize() {
-    return null;
+    ByteBuf b = Unpooled.buffer();
+    b.writeInt(_attacker.getID());
+    
+    if(_defender != null) {
+      b.writeInt(_defender != null ? _defender.getID() : 0);
+      b.writeInt(_damage);
+    } else b.writeInt(0);
+    
+    return b;
   }
   
   public void deserialize(ByteBuf data) throws NotEnoughDataException {
