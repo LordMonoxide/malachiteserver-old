cd8e02d6983c8335decfe91c452d0540555d721a
Corey Frenette <corey@narwhunderful.com>
2013-08-12 21:50:56 -0300

    Much better (but still not perfect) movement

--- code/src/game/world/EntityAI.java
+++ code/src/game/world/EntityAI.java
@@ -6,12 +6,12 @@ import game.data.util.Time;
 import game.network.packet.EntityMoveStart;
 import game.network.packet.EntityMoveStop;
 import game.pathfinding.AStar;
+import game.settings.Settings;
 
 public class EntityAI extends EntityLiving {
   private static Random _rand = new Random();
   
   private double _nextMove;
-  private double _nextMove2;
   private AStar.Node[] _nodes;
   private int _node;
   
@@ -20,53 +20,39 @@ public class EntityAI extends EntityLiving {
   }
   
   public void checkMovement() {
-    if(_nodes != null) {
-      if(_nextMove2 < Time.getTime()) {
-        if(_node <= _nodes.length - 2) {
-          if(_node < _nodes.length - 2) {
-            double x, y;
-            
-            if(_node >= 0) {
-              x = _nodes[_node + 1].getWorldX() - _nodes[_node].getWorldX();
-              y = _nodes[_node + 1].getWorldY() - _nodes[_node].getWorldY();
-            } else {
-              x = _nodes[_node + 1].getWorldX() - x();
-              y = _nodes[_node + 1].getWorldY() - y();
-            }
-            
-            double entityAngle = Math.atan2(y, x);
-            xy(_nodes[_node + 1].getWorldX(), _nodes[_node + 1].getWorldY());
-            bear((float)Math.toDegrees(entityAngle));
-            startMoving();
-            world().send(new EntityMoveStart(this));
-          } else {
-            xy(_nodes[_node + 1].getWorldX(), _nodes[_node + 1].getWorldY());
-            stopMoving();
-            world().send(new EntityMoveStop(this));
-          }
-        } else {
-          _nodes = null;
-          _nextMove = Time.getTime() + _rand.nextDouble() * 5000 + 5000;
-        }
-        
-        _node++;
-        /*if(_node <= _nodes.length - 1) {
-          x(_nodes[_node].getWorldX() + 8);
-          y(_nodes[_node].getWorldY() + 8);
-          world().send(new EntityMoveStop(this));
-        } else {
-          _nodes = null;
-          _nextMove = Time.getTime() + _rand.nextDouble() * 5000 + 5000;
-        }*/
-        
-        _nextMove2 = Time.getTime() + 75;
-      }
-    } else {
+    if(_nodes == null) {
       if(_nextMove < Time.getTime()) {
+        System.out.println("MOVING");
         float x = x() + _rand.nextFloat() * 400 - 200;
         float y = y() + _rand.nextFloat() * 400 - 200;
-        _nodes = world().findPath(this, x, y);
-        _node = -1;
+        
+        if((_nodes = world().findPath(this, x, y)) != null) {
+          _node = 0;
+          bear(_nodes[_node].angle());
+          startMoving();
+          world().send(new EntityMoveStart(this));
+        }
+        
+        _nextMove = Time.getTime() + _rand.nextDouble() * 5000 + 5000;
+      }
+    } else {
+      if(Math.floor(rx() / Settings.Map.Attrib.Size()) == _nodes[_node].x() && Math.floor(ry() / Settings.Map.Attrib.Size()) == _nodes[_node].y()) {
+        _node++;
+        System.out.println(_node + "/" + (_nodes.length - 1));
+        if(_node < _nodes.length - 1) {
+          System.out.println("ASDF");
+          bear(_nodes[_node].angle());
+          System.out.println((int)rx() / Settings.Map.Attrib.Size() + "\t" + (int)ry() / Settings.Map.Attrib.Size());
+          System.out.println(_nodes[_node].x() + "\t" + _nodes[_node].y());
+          System.out.println(bear());
+          world().send(new EntityMoveStart(this));
+        } else {
+          System.out.println("DONE");
+          xy(_nodes[_node].getWorldX(), _nodes[_node].getWorldY());
+          stopMoving();
+          world().send(new EntityMoveStop(this));
+          _nodes = null;
+        }
       }
     }
   }
